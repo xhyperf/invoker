1f0f7372a941414b5c641e0699f51c04a574849e
Qi <i@yangqi.co>
2025-10-16 11:50:18 +0800

    可绑定作用域，从而使用私有的策略执行方法

--- src/Invoker.php
+++ src/Invoker.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace XHyperf\Invoker;
 
+use Closure;
 use Hyperf\Context\ApplicationContext;
 
 use function Hyperf\Collection\data_get;
@@ -13,32 +14,34 @@ class Invoker
 {
     /**
      * 策略调用
-     * @param StrategyType|null $type    策略类型枚举
-     * @param array             $args    参数
-     * @param mixed|null        $default 默认值，策略执行方法不存在时返回
-     * @param string            $scope   作用域，用于区分同一策略类型枚举在不同场景下的调用
-     * @param bool              $strict  是否严格模式，为true时，空参数都会使用默认值
+     * @param ?StrategyType $type    策略类型枚举
+     * @param array         $args    参数
+     * @param mixed|null    $default 默认值，策略执行方法不存在时返回
+     * @param string        $scope   作用域，用于区分同一策略类型枚举在不同场景下的调用
+     * @param bool          $bind    是否绑定作用域，为true时，会将调用方法绑定到 `$scope`，此时使用 **私有** 策略执行方法
+     * @param bool          $strict  是否严格模式，为true时，空参数都会使用默认值
      * @return mixed
      */
-    public static function strategy(?StrategyType $type, array $args = [], mixed $default = null, string $scope = '', bool $strict = false): mixed
+    public static function strategy(?StrategyType $type, array $args = [], mixed $default = null, string $scope = '', bool $bind = false, bool $strict = false): mixed
     {
         if (! $handler = StrategyCollector::getHandler($type, $scope)) {
             return value($default, $args);
         }
 
-        return static::reflect($handler[0], $args, $handler[1], $strict);
+        return static::reflect($handler[0], $args, $handler[1], scope: $bind ? $scope : '', strict: $strict);
     }
 
     /**
      * 自动适配参数的反射调用
-     * @param array $callable callable 调用数组
-     * @param array $args     参数
-     * @param array $mapper   参数映射，格式 源参数 key => 转换后的目标参数 key
-     * @param bool  $strict   是否严格模式，为true时，空参数都会使用默认值
+     * @param array  $callable callable 调用数组
+     * @param array  $args     参数
+     * @param array  $mapper   参数映射，格式 源参数 key => 转换后的目标参数 key
+     * @param string $scope    绑定的作用域，绑定后可调用 **保护** 方法
+     * @param bool   $strict   是否严格模式，为true时，空参数都会使用默认值
      * @return mixed
      * @throws
      */
-    public static function reflect(array $callable, array $args = [], array $mapper = [], bool $strict = false): mixed
+    public static function reflect(array $callable, array $args = [], array $mapper = [], string $scope = '', bool $strict = false): mixed
     {
         [$class, $method] = $callable;
 
@@ -70,6 +73,10 @@ class Invoker
             $val = data_get($argsMapper, $name, $default);
 
             $parameters[] = $strict ? ($val ?: $default) : $val;
+        }
+
+        if ($scope) {
+            return Closure::bind(fn() => $class->{$method}(...$parameters), null, $scope)();
         }
 
         return $class->{$method}(...$parameters);
